d1744d106226bfbb64ad17514bdec97d4ed91c72
unbreak-jiv <unbreakstop@gmail.com>
2025-12-22 23:05:19 +0530

    update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const nextBtn = document.getElementById("nextBtn");
 // Cloudinary config
 const CLOUD_NAME = "dqmcqdtxn";
 const UPLOAD_PRESET = "orina_birthday";
-const MAKE_WEBHOOK_URL = "https://hook.eu2.make.com/ywszf6rc5ymdw1evmut2jx4316x8t2cc";
+const MAKE_WEBHOOK_URL = "https://hook.eu2.make.com/qigqaow6gli2oulrippv4rcb9novog4h";
 
 // Submission deadline
 const DEADLINE = new Date("2025-12-25T23:59:00");
