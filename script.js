52f7bb86f826b0afe7e63b5e34bff95038ee9355
unbreak-jiv <unbreakstop@gmail.com>
2026-01-12 00:45:47 +0530

    update script.js

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ const UPLOAD_PRESET = "orina_birthday";
 const MAKE_WEBHOOK_URL = "https://hook.eu2.make.com/qigqaow6gli2oulrippv4rcb9novog4h";
 
 // Submission deadline
-const DEADLINE = new Date("2026-01-05T12:00:00");
+const DEADLINE = new Date("2026-01-12T12:00:00");
 
 // State 
 let mediaRecorder;
